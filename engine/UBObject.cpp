5c88e8760310db4bef23c45948cc32d0cc79f05b
Jalil Modares <sayedjal@buffalo.edu>
2015-11-29 22:10:12 -0500

    Update UBObject.cpp

--- engine/UBObject.cpp
+++ engine/UBObject.cpp
@@ -5,6 +5,7 @@
 
 #include <QTimer>
 #include <QProcess>
+#include <QTcpSocket>
 
 #include "UASInterface.h"
 
@@ -13,7 +14,6 @@
 
 UBObject::UBObject(QObject *parent) : QObject(parent),
     m_uav(NULL),
-    m_link(0),
     m_cr(0),
     m_vr(0)
 {
@@ -23,8 +23,17 @@ UBObject::UBObject(QObject *parent) : QObject(parent),
     m_net_server = new UBServer(this);
     m_snr_server = new UBServer(this);
 
+    connect(m_net_server, SIGNAL(clientConnected(quint16)), this, SLOT(netClientConnectedEvent(quint16)));
     connect(m_net_server, SIGNAL(dataReady(QByteArray)), this, SLOT(netDataReadyEvent(QByteArray)));
+
+    connect(m_snr_server, SIGNAL(clientConnected(quint16)), this, SLOT(snrClientConnectedEvent(quint16)));
     connect(m_snr_server, SIGNAL(dataReady(QByteArray)), this, SLOT(snrDataReadyEvent(QByteArray)));
+
+    m_socket = new QTcpSocket(this);
+
+    connect(m_socket, SIGNAL(connected()), this, SLOT(connectedEvent()));
+    connect(m_socket, SIGNAL(disconnected()), this, SLOT(disconnectedEvent()));
+    connect(m_socket, SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(errorEvent(QAbstractSocket::SocketError)));
 
     m_timer = new QTimer(this);
     m_timer->setInterval(OBJECT_TRACK_RATE);
@@ -45,26 +54,47 @@ void UBObject::setAgent(const QString& path, const QStringList& args) {
 }
 
 void UBObject::startObject(int port) {
+    m_port = port;
+
     m_firmware->start();
-    m_link = LinkManagerFactory::addTcpConnection(QHostAddress::LocalHost, "", port + 2, false);
 
-    m_net_server->startServer((PHY_PORT - MAV_PORT) + port);
-    m_snr_server->startServer((SNR_PORT - MAV_PORT) + port);
+    m_net_server->startServer((PHY_PORT - MAV_PORT) + m_port);
+    m_snr_server->startServer((SNR_PORT - MAV_PORT) + m_port);
 
-    QTimer::singleShot(CONNECT_WAIT, this, SLOT(startAgent()));
-}
-
-void UBObject::startAgent() {
-    m_agent->start();
-
-    QTimer::singleShot(CONNECT_WAIT, this, SLOT(startConnection()));
-}
-
-void UBObject::startConnection() {
-    LinkManager::instance()->connectLink(m_link);
-
-    m_timer->start();
+    m_socket->connectToHost(QHostAddress::LocalHost, m_port);
 }
 
 void UBObject::objectTracker() {
+}
+
+void UBObject::connectedEvent() {
+    m_socket->disconnectFromHost();
+}
+
+void UBObject::disconnectedEvent() {
+    if (m_port % 10) {
+        int link = LinkManagerFactory::addTcpConnection(QHostAddress::LocalHost, "", m_port, false);
+        LinkManager::instance()->connectLink(link);
+
+        m_timer->start();
+    } else {
+        m_port += 2;
+
+        m_agent->start();
+        m_socket->connectToHost(QHostAddress::LocalHost, m_port);
+    }
+}
+
+void UBObject::errorEvent(QAbstractSocket::SocketError err) {
+    if (err == QAbstractSocket::ConnectionRefusedError) {
+        m_socket->connectToHost(QHostAddress::LocalHost, m_port);
+    }
+}
+
+void UBObject::netClientConnectedEvent(quint16 port) {
+    QLOG_INFO() << "PHY Connected | Port: " << port;
+}
+
+void UBObject::snrClientConnectedEvent(quint16 port) {
+    QLOG_INFO() << "SNR Connected | Port: " << port;
 }
